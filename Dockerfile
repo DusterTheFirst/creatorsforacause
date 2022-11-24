89cf4a2ce64ce73b76aeeb7b233b595d80470ed7
Zachary Kohnen <me@dusterthefirst.com>
2022-11-24 14:08:45 +0100

    Fly deploy

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,25 @@
-FROM rust as builder
+FROM lukemathwalker/cargo-chef:latest-rust-1 AS chef
+RUN apt update && apt install -y protobuf-compiler
 WORKDIR /app
 
-COPY . /app
+FROM chef AS planner
+COPY . .
+RUN cargo chef prepare --recipe-path recipe.json
+
+FROM chef AS builder
+COPY --from=planner /app/recipe.json recipe.json
+# Build dependencies - this is the caching Docker layer!
+RUN cargo chef cook --release --recipe-path recipe.json
+
+# Build application
+COPY . .
 RUN cargo build --release
 
-FROM gcr.io/distroless/cc
+FROM gcr.io/distroless/cc AS runtime
 
 COPY --from=builder \
     /app/target/release/creatorsforacause \
     /
+COPY creators.toml .
 
 CMD [ "/creatorsforacause" ]
--- src/web.rs
+++ src/web.rs
@@ -1,19 +1,20 @@
 use std::net::SocketAddr;
 
 use axum::{extract::State, handler::Handler, routing::get, Json, Router, Server};
-use color_eyre::eyre::{bail, Context};
+use color_eyre::eyre::Context;
 use serde_json::{json, Value};
 use tokio::sync::watch;
 use tracing::info;
 
 use crate::youtube::YoutubeLiveStreams;
 
+#[tracing::instrument(skip(youtube_status))]
 pub async fn web_server(
     listen: SocketAddr,
     youtube_status: watch::Receiver<YoutubeLiveStreams>,
 ) -> color_eyre::Result<()> {
     let app = Router::new()
-        .route("/", get(|| async { "Hello, World!" }))
+        .route("/", get(|| async { "OK" }))
         .route_service("/status", status.with_state(youtube_status));
 
     info!("Starting web server on http://{listen}");
